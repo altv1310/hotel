da3bc5d806efa3ac3d3c4c697f8109cc51e91add
altv1310 <altv1310@chawk.cecil.edu>
2021-10-02 10:21:20 -0400

    Added Room model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+BASE_PRICE = 200.00
+SUITE_UPCHARGE = .25
+OCEAN_UPCHARGE = .4
+
+
 # Create your models here.
+class Room(models.Model):
+    room_num = models.IntegerField(default=0)
+    occupied = models.BooleanField(default=False)
+    oceanside = models.BooleanField(default=False)
+    suite = models.BooleanField(default=False)
+    if suite:
+        price = models.FloatField(default=BASE_PRICE)
+
+    def __str__(self):
+        return f'Room#{self.room_num}'
--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+ROOMS = [101, 102, 103, 104]
 
 
 def home(request):
     return render(request, 'booking/home.html', context={
         'user_name': 'Steve',
-        'available_rooms': [101, 102, 103, 104]
+        'available_rooms': ROOMS
     })
